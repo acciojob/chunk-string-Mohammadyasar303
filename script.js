ef628bb63c541845bbbf660f9445eed51319185b
Mohammadyasar Mansuri <126454357+Mohammadyasar303@users.noreply.github.com>
2025-02-06 12:26:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function stringChop(str, size) {
-  // your code here
+let ans = [];
+	if (!str) { 
+        return ans;
+    }
+    
+for (let i = 0; i < str.length; i += size) {
+	let newStr=str.slice(i, i + size)
+    result.push(newStr);
+}    
+console.log(result);
 }
 
 // Do not change the code below
